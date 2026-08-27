b99f6b1d3f506375ab05f7d60cc066fa76ea76d1
enricosavorgnan <enricosavorgnan@gmail.com>
2026-08-27 16:22:45 +0200

    runners: add sbatch file to runners folder

---
+++ runners/sbatch-exp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=NBODY
+#SBATCH --output=../../log/nbody_%j.out
+#SBATCH --partition=GENOA
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=64G
+#SBATCH --time=02:00:00
+
+cd "../experiments/rsqrt-impact/"
+mkdir -p ../../log
+
+make clean
+make all PRECISION=double
+make test-serial
--- runners/sbatch.sh
+++ runners/sbatch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=NBODY
-#SBATCH --output=log/nbody_%j.out
+#SBATCH --output=../log/nbody_%j.out
 #SBATCH --partition=GENOA
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -9,10 +9,8 @@
 #SBATCH --mem=64G
 #SBATCH --time=02:00:00
 
-if [ -n "$SLURM_SUBMIT_DIR" ]; then
-    cd "$SLURM_SUBMIT_DIR"
-fi
-mkdir -p log
+cd "../src/"
+mkdir -p ../log
 
 make clean
 make all PRECISION=double
