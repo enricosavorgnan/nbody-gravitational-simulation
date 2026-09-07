e553e0e48489bda24ac62730f1b8f15a353a4187
enricosavorgnan <enricosavorgnan@gmail.com>
2026-09-07 17:02:52 +0200

    exp: setup Makefiles for final experiments

---
+++ runners/sbatch-kernel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --account=enricosavorgnan
+#SBATCH --job-name=KERNEL
+#SBATCH --output=./log/kernel_%j.out
+#SBATCH --error=./log/kernel_%j.err
+#SBATCH --partition=GENOA
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=64G
+#SBATCH --time=02:00:00
+
+cd "$SLURM_SUBMIT_DIR"
+cd "./experiments/kernel"
+
+make test-kernel USE_PAPI=1
