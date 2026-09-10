de47643eaf693195ed4b3cbd46520fae21c93a7d
enricosavorgnan <enricosavorgnan@gmail.com>
2026-09-10 22:13:11 +0200

    runners: add MPI runners

--- experiments/mpi/headers/integration.h
+++ experiments/mpi/headers/integration.h
@@ -28,68 +28,31 @@ typedef void (*kernel_t)(
 
 
 void compute_accelerations_omp_br(const size_t  n,
-                              const dtype   g,
-                              const dtype   mass,
-                              const dtype   eps,
-                              const dtype   * restrict x,
-                              const dtype   * restrict y,
-                              const dtype   * restrict z,
-                              dtype   * restrict ax,
-                              dtype   * restrict ay,
-                              dtype   * restrict az
-                  );
+                                  const dtype   g,
+                                  const dtype   mass,
+                                  const dtype   eps,
+                                  const dtype * restrict x,
+                                  const dtype * restrict y,
+                                  const dtype * restrict z,
+                                  dtype * restrict ax,
+                                  dtype * restrict ay,
+                                  dtype * restrict az
+                                  );
 
-
-void compute_accelerations_omp_rt(const size_t  n,
-                              const dtype   g,
-                              const dtype   mass,
-                              const dtype   eps,
-                              const dtype   * restrict x,
-                              const dtype   * restrict y,
-                              const dtype   * restrict z,
-                              dtype   * restrict ax,
-                              dtype   * restrict ay,
-                              dtype   * restrict az
-                  );
-
-
-void compute_accelerations_omp_brt(const size_t  n,            // number of particles
-                                    const dtype   g,                            // gravitational constant
-                                      const dtype   mass,                         // mass of every source particle
-                                      const dtype   eps,                          // Plummer softening length
-                                      const dtype   * restrict x,                           // x positions, read-only
-                                      const dtype   * restrict y,                           // y positions, read-only
-                                      const dtype   * restrict z,                           // z positions, read-only
-                                      dtype   * restrict ax,                                // x acceleration, overwritten
-                                      dtype   * restrict ay,                                // y acceleration, overwritten
-                                      dtype   * restrict az                                 // z acceleration, overwritten
-                  );
-
-
-void compute_accelerations_omp_rt_red(const size_t  n,
-                              const dtype   g,
-                              const dtype   mass,
-                              const dtype   eps,
-                              const dtype   * restrict x,
-                              const dtype   * restrict y,
-                              const dtype   * restrict z,
-                              dtype   * restrict ax,
-                              dtype   * restrict ay,
-                              dtype   * restrict az
-                  );
-
-
-void compute_accelerations_omp_brt_red(const size_t  n,            // number of particles
-                                    const dtype   g,                            // gravitational constant
-                                      const dtype   mass,                         // mass of every source particle
-                                      const dtype   eps,                          // Plummer softening length
-                                      const dtype   * restrict x,                           // x positions, read-only
-                                      const dtype   * restrict y,                           // y positions, read-only
-                                      const dtype   * restrict z,                           // z positions, read-only
-                                      dtype   * restrict ax,                                // x acceleration, overwritten
-                                      dtype   * restrict ay,                                // y acceleration, overwritten
-                                      dtype   * restrict az                                 // z acceleration, overwritten
-                  );
+void compute_accelerations_omp_br_cross(const size_t  local_n,
+                                        const size_t  visit_n,
+                                        const dtype   g,
+                                        const dtype   mass,
+                                        const dtype   eps,
+                                        const dtype * restrict local_x,
+                                        const dtype * restrict local_y,
+                                        const dtype * restrict local_z,
+                                        const dtype * restrict visit_x,
+                                        const dtype * restrict visit_y,
+                                        const dtype * restrict visit_z,
+                                        dtype * restrict ax,
+                                        dtype * restrict ay,
+                                        dtype * restrict az);
 
 
 void drift (particles_t *p,                                                    // particle positions are modified in place
