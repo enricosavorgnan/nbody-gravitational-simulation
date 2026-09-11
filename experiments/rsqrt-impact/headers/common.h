5f2bafa366cd86c47f27b2311ff7c6e63e6a0652
enricosavorgnan <enricosavorgnan@gmail.com>
2026-09-11 16:22:54 +0200

    exp/rsqrt: improve RSQRT method when loops

--- experiments/rsqrt-impact/headers/common.h
+++ experiments/rsqrt-impact/headers/common.h
@@ -46,6 +46,7 @@ static inline dtype dtype_sqrt (dtype x)
 // Fast Inverse Square Root, taken from Wikipedia:
 // https://en.wikipedia.org/wiki/Fast_inverse_square_root
 // it should be correct up to the 11th bit
+#if N_RSQRT_LOOP != 1
 static inline dtype dtype_rsqrt (dtype x)
 {
   union {
@@ -61,25 +62,17 @@ static inline dtype dtype_rsqrt (dtype x)
   }
   return y;
 }
-
-// The following is the implementation
-// of Fast Inverse Square Root using AVX-512F instructions
-static inline dtype dtype_rsqrt_old (dtype x)
+#else
+static inline dtype dtype_rsqrt (dtype x)
 {
-  // 1. Load scalar float into the lowest element of a 128-bit vector register
-  __m128 vec = _mm_set_ss(x);
+  union {
+    uint32_t i;
+    float f;
+  } u = { .f = x };
 
-  // 2. Hardware AVX-512F approximation (14 bits of precision)
-  vec = _mm_rsqrt14_ss(vec, vec);
-
-  // 3. Extract back to scalar
-  dtype y = _mm_cvtss_f32(vec);
-
-  // 4. Newton-Raphson refinement
-  for (int l = 0; l < N_RSQRT_LOOP; l++)
-  {
-    y = y * ((dtype) 1.5 - (dtype) 0.5 * x * y * y);
-  }
+  u.i = 0x5f3759dfu - (u.i >> 1);
+  dtype y = u.f;
+  y = y * ((dtype) 1.5 - (dtype) 0.5 * x * y * y);
   return y;
 }
 
@@ -200,5 +193,107 @@ static inline bool dtype_isfinite (dtype x)
 {
   return isfinite ((double) x);
 }
+
+#else
+typedef double dtype;
+#define DTYPE_NAME "double"
+#define DTYPE_MAX_VALUE DBL_MAX
+#define DTYPE_MIN_NORMAL DBL_MIN
+#define DTYPE_PRINTF_FORMAT "%.17g"
+
+static inline dtype dtype_sqrt (dtype x)
+{
+  return sqrt (x);
+}
+
+#if N_RSQRT_LOOP != 1
+static inline dtype dtype_rsqrt (dtype x)
+{
+  union {
+    uint64_t i;
+    double f;
+  } u = { .f = x };
+
+  u.i = UINT64_C (0x5fe6eb50c7b537a9) - (u.i >> 1);
+  dtype y = u.f;
+  for (int l = 0; l < N_RSQRT_LOOP; l++)
+  {
+    y = y * ((dtype) 1.5 - (dtype) 0.5 * x * y * y);
+  }
+  return y;
+}
+#else
+static inline dtype dtype_rsqrt (dtype x)
+{
+  union {
+    uint64_t i;
+    double f;
+  } u = { .f = x };
+
+  u.i = UINT64_C (0x5fe6eb50c7b537a9) - (u.i >> 1);
+  dtype y = u.f;
+  y = y * ((dtype) 1.5 - (dtype) 0.5 * x * y * y);
+  return y;
+}
+
+static inline dtype dtype_rsqrt_old (dtype x)
+{
+  // 1. Load scalar double into the lowest element of a 128-bit vector register
+  __m128d vec = _mm_set_sd(x);
+
+  // 2. Hardware AVX-512F approximation for double precision (14 bits of precision)
+  vec = _mm_rsqrt14_sd(vec, vec);
+
+  // 3. Extract back to scalar
+  dtype y = _mm_cvtsd_f64(vec);
+
+  // 4. Newton-Raphson refinement
+  for (int l = 0; l < N_RSQRT_LOOP; l++)
+  {
+    y = y * ((dtype) 1.5 - (dtype) 0.5 * x * y * y);
+  }
+  return y;
+}
+
+static inline dtype dtype_pow (dtype x,
+                               dtype y)
+{
+  return pow (x, y);
+}
+
+static inline dtype dtype_sin (dtype x)
+{
+  return sin (x);
+}
+
+static inline dtype dtype_cos (dtype x)
+{
+  return cos (x);
+}
+
+static inline dtype dtype_log (dtype x)
+{
+  return log (x);
+}
+
+static inline dtype dtype_fabs (dtype x)
+{
+  return fabs (x);
+}
+
+static inline dtype dtype_fmax (dtype x,
+                                dtype y)
+{
+  return fmax (x, y);
+}
+
+#endif
+
+static inline bool dtype_isfinite (dtype x)
+{
+  return isfinite ((double) x);
+}
+
+#endif
 
 #endif
