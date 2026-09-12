16e66094d0ac826bec53dc1c4b318b55683892c3
enricosavorgnan <enricosavorgnan@gmail.com>
2026-09-12 17:33:08 +0200

    exp/loop: update Makefile to run experiments with different N

---
+++ runners/sbatch-loop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=UNROLL1
+#SBATCH --output=./log/unroll1_%j.out
+#SBATCH --error=./log/unroll1_%j.err
+#SBATCH --partition=GENOA
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=64G
+#SBATCH --time=02:00:00
+
+cd "$SLURM_SUBMIT_DIR"
+cd "./experiments/loop-unroll"
+
+make test-8-unroll USE_PAPI=1
+make test-4-unroll USE_PAPI=1
+make test-2-unroll USE_PAPI=1
+make test-auto-unroll USE_PAPI=1
